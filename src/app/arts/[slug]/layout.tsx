c539f23d455065a8cb8e19d646d882630c046e8f
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-07-07 10:37:56 +0700

    chore: remove margin bottom class

--- src/app/arts/[slug]/layout.tsx
+++ src/app/arts/[slug]/layout.tsx
@@ -41,7 +41,7 @@ export default async function RootLayout({
 	return (
 		<>
 			{art && (
-				<div className="w-full max-w-5xl mx-auto mb-6">
+				<div className="w-full max-w-5xl mx-auto">
 					<Link href="/arts" className="inline-block">
 						<Button variant="ghost" className="flex items-center gap-2 text-2xl text-gray-500 hover:text-gray-700 transition-colors">
 							<ArrowLeftCircle className="w-8 h-8" />
