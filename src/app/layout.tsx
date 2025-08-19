14cc809b463a4a9198668338f96356fbed0f27cb
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-08-19 19:39:32 +0700

    chore: add meta data to support seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,31 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
 	title: "Diva Nur Anggraeni",
-	description: "Webnya diva",
+	description: "Saya adalah seorang remaja asal Purwokerto yang memiliki hobi menggambar sejak kecil. Seiring waktu, saya mulai menekuni seni menggambar, khususnya dengan media pensil, karena saya tertarik pada detail, tekstur, dan ekspresi yang bisa disampaikan lewat garis hitam-putih.",
+	keywords: ["Diva Nur Anggraeni", "Diva", "Artist", "High School Artist", "Portfolio"],
+	authors: [{ name: "Diva Nur Anggraeni" }],
+	creator: "Diva Nur Anggraeni",
+	openGraph: {
+		title: "Diva Nur Anggraeni - Web Developer Portfolio",
+		description: "Explore the personal portfolio of Diva Nur Anggraeni, a high school student passionate about drawing.",
+		url: "",
+		siteName: "Diva Nur Anggraeni Portfolio",
+		type: "website",
+		images: [
+			{
+				url: "",
+				width: 1200,
+				height: 630,
+				alt: "Diva Nur Anggraeni Portfolio",
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Diva Nur Anggraeni - Web Developer Portfolio",
+		description: "Discover Diva's projects and skills in pencil drawing.",
+		images: [""],
+	},
 }
 
 export default function RootLayout({
